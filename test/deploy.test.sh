5670a17b4aff8c29afba708bba530cfa84ede3fc
Christian Dielitz <christian@dielitz.net>
2023-11-19 21:45:58 +0100

    Improved tests

--- test/deploy.test.sh
+++ test/deploy.test.sh
@@ -4,16 +4,16 @@ FILE_NAME="${LEGO_CERT_DOMAIN#\*.}"
 
 # If file exists this was a renew call, stop container
 if test -f /app/certificates/run_$FILE_NAME.crt; then
-    cp -f $LEGO_CERT_PATH /app/certificates/renew_$FILE_NAME.crt
-    cp -f $LEGO_CERT_KEY_PATH /app/certificates/renew_$FILE_NAME.key
+    cp -f $LEGO_CERT_PATH /app/certificates/new_$FILE_NAME.crt
+    cp -f $LEGO_CERT_KEY_PATH /app/certificates/new_$FILE_NAME.key
     
-    # Stop pebble. Since started with "abort-on-container-exit" this container will also stop
+    # Stop pebble. Since docker is started with "exit-code-from" this container will also stop
     curl --unix-socket /var/run/docker.sock -X POST http://localhost/containers/pebble/stop
 else
     cp -f $LEGO_CERT_PATH /app/certificates/run_$FILE_NAME.crt
     cp -f $LEGO_CERT_KEY_PATH /app/certificates/run_$FILE_NAME.key
 
-    sleep 60s
+    sleep 10s
 
     # Trigger renew
     sh /app/renew
